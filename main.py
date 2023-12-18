14099229be7bcc798ce6ff27e984f6b3c4f9a6a4
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-12-18 12:21:57 +0000

    base game

--- main.py
+++ main.py
@@ -10,75 +10,83 @@ turn_speed = 0.2
 speed = 0
 rotation = 0
 
-# sprites 
+# sprites
 ship = sprites.create(assets.image("ship"), SpriteKind.player)
 transformSprites.rotate_sprite(ship, 90)
 
 # setup
-tiles.set_current_tilemap(assets.tilemap("test"))
+tiles.set_current_tilemap(assets.tilemap("level"))
 scene.camera_follow_sprite(ship)
 
-def spawn_treasure():
+def spawn_treasure(tile):
+    treasure = sprites.create(assets.image("x"), SpriteKind.treasure)
+    hitbox = sprites.create(image.create(47, 47), SpriteKind.hitbox)
+    spriteutils.draw_circle(hitbox.image, 24, 24, 20, 1)
+    hitbox.set_flag(SpriteFlag.INVISIBLE, True)
+    tiles.place_on_tile(treasure, tile)
+    tiles.place_on_tile(hitbox, tile)
+    sprites.set_data_sprite(hitbox, "treasure", treasure)
+
+def spawn_fort(tile):
+    fort = sprites.create(assets.image("fort"), SpriteKind.enemy)
+    tiles.place_on_tile(fort, tile)
+    bar = statusbars.create(20, 4, StatusBarKind.enemy_health)
+    bar.attach_to_sprite(fort)
+
+def spawn_loop():
     if len(sprites.all_of_kind(SpriteKind.treasure)) < 10:
-        treasure = sprites.create(assets.image("x"), SpriteKind.treasure)
-        hitbox = sprites.create(image.create(47, 47), SpriteKind.hitbox)
-        spriteutils.draw_circle(hitbox.image, 24, 24, 20, 1)
-        hitbox.set_flag(SpriteFlag.INVISIBLE, True)
         tile = tilesAdvanced.get_all_wall_tiles()._pick_random()
-        tiles.place_on_tile(treasure, tile)
-        tiles.place_on_tile(hitbox, tile)
-        sprites.set_data_sprite(hitbox, "treasure", treasure)
-game.on_update_interval(2000, spawn_treasure)
+        spawn_treasure(tile)
+    if len(sprites.all_of_kind(SpriteKind.enemy)) < 10:
+        tile = tilesAdvanced.get_all_wall_tiles()._pick_random()
+        spawn_fort(tile)
+game.on_update_interval(2000, spawn_loop)
+
+def make_projectile(source: Sprite, kind):
+    proj = sprites.create(assets.image("cannon ball"), kind)
+    proj.set_position(source.x, source.y)
+    proj.lifespan = 1500
+    proj.set_flag(SpriteFlag.GHOST_THROUGH_WALLS, True)
+    return proj
+
+def player_fire():
+    for rotation in range(0, 181, 180):
+        proj = make_projectile(ship, SpriteKind.projectile)
+        angle = transformSprites.get_rotation(ship) + rotation
+        angle = spriteutils.degrees_to_radians(angle)
+        spriteutils.set_velocity_at_angle(proj, angle, 100)
+controller.A.on_event(ControllerButtonEvent.PRESSED, player_fire)
+
+def enemy_fire(fort: Sprite):
+    if randint(1, 100) == 1 and spriteutils.distance_between(fort, ship) < 80:
+        proj = make_projectile(fort, SpriteKind.enemy_projectile)
+        angle = spriteutils.angle_from(fort, ship)
+        spriteutils.set_velocity_at_angle(proj, angle, 100)
 
 def collect_treasure(ship, treasure_hitbox):
     info.change_score_by(randint(500, 2000))
     sprites.read_data_sprite(treasure_hitbox, "treasure").destroy()
     treasure_hitbox.destroy()
-    spawn_treasure()
 sprites.on_overlap(SpriteKind.player, SpriteKind.hitbox, collect_treasure)
 
-def fire(source: Sprite, spritekind):
-    for rotation in range(0, 181, 180):
-        proj = sprites.create(assets.image("cannon ball"), spritekind)
-        proj.set_position(source.x, source.y)
-        proj.set_flag(SpriteFlag.DESTROY_ON_WALL, True)
-        angle = transformSprites.get_rotation(source) + rotation
-        angle = spriteutils.degrees_to_radians(angle)
-        spriteutils.set_velocity_at_angle(proj, angle, 100)
-
-def player_fire():
-    fire(ship, SpriteKind.projectile)
-controller.A.on_event(ControllerButtonEvent.PRESSED, player_fire)
-
-def spawn_enemies():
-    if len(sprites.all_of_kind(SpriteKind.enemy)) < 10:
-        enemy = tilesAdvanced.create_pathfinder_sprite(assets.image("enemy"), SpriteKind.enemy)
-        tilesAdvanced.place_on_random_tile_off_screen(enemy, assets.tile("water"))
-        tilesAdvanced.follow_using_pathfinding(enemy, ship, 20)
-# game.on_update_interval(2000, spawn_enemies)
-spawn_enemies()
-
-def enemy_behaviour(enemy: PathfindingSprite):
-    angle_in_rads = spriteutils.angle_from(enemy, ship)
-    angle_in_degrees = spriteutils.radians_to_degrees(angle_in_rads)
-    if spriteutils.distance_between(enemy, ship) > 80:
-        transformSprites.rotate_sprite(enemy, angle_in_degrees + 90)
-    else:
-        tilesAdvanced.stop_following_path(enemy)
-        if transformSprites.get_rotation(enemy) - angle_in_degrees > 1:
-            transformSprites.change_rotation(enemy, -0.5)
-        if randint(1, 100) == 1:
-            fire(enemy, SpriteKind.enemy_projectile)
-
-def destroy_enemy(cannon_ball, enemy):
+def hit_fort(fort, cannon_ball):
+    bar = statusbars.get_status_bar_attached_to(StatusBarKind.enemy_health, fort)
+    bar.value -= 10
     cannon_ball.destroy()
-    enemy.destroy()
-sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, destroy_enemy)
+    if bar.value < 1:
+        spawn_treasure(fort.tilemap_location())
+        fort.destroy()
+sprites.on_overlap(SpriteKind.enemy, SpriteKind.projectile, hit_fort)
 
-def hit():
+def player_hit(player, cannon_ball):
     info.change_life_by(-1)
+    cannon_ball.destroy()
     pause(1000)
-sprites.on_overlap(SpriteKind.player, SpriteKind.enemy_projectile, hit)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy_projectile, player_hit)
+
+def fix_double_spawn(enemy, other_enemy):
+    sprites.all_of_kind(SpriteKind.enemy).pop().destroy()
+sprites.on_overlap(SpriteKind.enemy, SpriteKind.enemy, fix_double_spawn)
 
 def turn_ship():
     global rotation
@@ -103,10 +111,5 @@ def tick():
     turn_ship()
     move()
     for enemy in sprites.all_of_kind(SpriteKind.enemy):
-        enemy_behaviour(enemy)
-    info.change_score_by(1)
+        enemy_fire(enemy)
 game.on_update(tick)
-
-
-# treasure map
-# selling mechanic
--- main.ts
+++ main.ts
@@ -9,90 +9,93 @@ let ship_acceleration = 1.5
 let turn_speed = 0.2
 let speed = 0
 let rotation = 0
-//  sprites 
+//  sprites
 let ship = sprites.create(assets.image`ship`, SpriteKind.Player)
 transformSprites.rotateSprite(ship, 90)
 //  setup
-tiles.setCurrentTilemap(assets.tilemap`test`)
+tiles.setCurrentTilemap(assets.tilemap`level`)
 scene.cameraFollowSprite(ship)
-function spawn_treasure() {
-    let treasure: Sprite;
-    let hitbox: Sprite;
+function spawn_treasure(tile: any) {
+    let treasure = sprites.create(assets.image`x`, SpriteKind.treasure)
+    let hitbox = sprites.create(image.create(47, 47), SpriteKind.hitbox)
+    spriteutils.drawCircle(hitbox.image, 24, 24, 20, 1)
+    hitbox.setFlag(SpriteFlag.Invisible, true)
+    tiles.placeOnTile(treasure, tile)
+    tiles.placeOnTile(hitbox, tile)
+    sprites.setDataSprite(hitbox, "treasure", treasure)
+}
+
+function spawn_fort(tile: any) {
+    let fort = sprites.create(assets.image`fort`, SpriteKind.Enemy)
+    tiles.placeOnTile(fort, tile)
+    let bar = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
+    bar.attachToSprite(fort)
+}
+
+game.onUpdateInterval(2000, function spawn_loop() {
     let tile: tiles.Location;
     if (sprites.allOfKind(SpriteKind.treasure).length < 10) {
-        treasure = sprites.create(assets.image`x`, SpriteKind.treasure)
-        hitbox = sprites.create(image.create(47, 47), SpriteKind.hitbox)
-        spriteutils.drawCircle(hitbox.image, 24, 24, 20, 1)
-        hitbox.setFlag(SpriteFlag.Invisible, true)
         tile = tilesAdvanced.getAllWallTiles()._pickRandom()
-        tiles.placeOnTile(treasure, tile)
-        tiles.placeOnTile(hitbox, tile)
-        sprites.setDataSprite(hitbox, "treasure", treasure)
+        spawn_treasure(tile)
+    }
+    
+    if (sprites.allOfKind(SpriteKind.Enemy).length < 10) {
+        tile = tilesAdvanced.getAllWallTiles()._pickRandom()
+        spawn_fort(tile)
+    }
+    
+})
+function make_projectile(source: Sprite, kind: number): Sprite {
+    let proj = sprites.create(assets.image`cannon ball`, kind)
+    proj.setPosition(source.x, source.y)
+    proj.lifespan = 1500
+    proj.setFlag(SpriteFlag.GhostThroughWalls, true)
+    return proj
+}
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function player_fire() {
+    let proj: Sprite;
+    let angle: number;
+    for (let rotation = 0; rotation < 181; rotation += 180) {
+        proj = make_projectile(ship, SpriteKind.Projectile)
+        angle = transformSprites.getRotation(ship) + rotation
+        angle = spriteutils.degreesToRadians(angle)
+        spriteutils.setVelocityAtAngle(proj, angle, 100)
+    }
+})
+function enemy_fire(fort: Sprite) {
+    let proj: Sprite;
+    let angle: number;
+    if (randint(1, 100) == 1 && spriteutils.distanceBetween(fort, ship) < 80) {
+        proj = make_projectile(fort, SpriteKind.enemy_projectile)
+        angle = spriteutils.angleFrom(fort, ship)
+        spriteutils.setVelocityAtAngle(proj, angle, 100)
     }
     
 }
 
-game.onUpdateInterval(2000, spawn_treasure)
 sprites.onOverlap(SpriteKind.Player, SpriteKind.hitbox, function collect_treasure(ship: Sprite, treasure_hitbox: Sprite) {
     info.changeScoreBy(randint(500, 2000))
     sprites.readDataSprite(treasure_hitbox, "treasure").destroy()
     treasure_hitbox.destroy()
-    spawn_treasure()
 })
-function fire(source: Sprite, spritekind: number) {
-    let proj: Sprite;
-    let angle: number;
-    for (let rotation = 0; rotation < 181; rotation += 180) {
-        proj = sprites.create(assets.image`cannon ball`, spritekind)
-        proj.setPosition(source.x, source.y)
-        proj.setFlag(SpriteFlag.DestroyOnWall, true)
-        angle = transformSprites.getRotation(source) + rotation
-        angle = spriteutils.degreesToRadians(angle)
-        spriteutils.setVelocityAtAngle(proj, angle, 100)
-    }
-}
-
-controller.A.onEvent(ControllerButtonEvent.Pressed, function player_fire() {
-    fire(ship, SpriteKind.Projectile)
-})
-function spawn_enemies() {
-    let enemy: tilesAdvanced.PathfinderSprite;
-    if (sprites.allOfKind(SpriteKind.Enemy).length < 10) {
-        enemy = tilesAdvanced.createPathfinderSprite(assets.image`enemy`, SpriteKind.Enemy)
-        tilesAdvanced.placeOnRandomTileOffScreen(enemy, assets.tile`water`)
-        tilesAdvanced.followUsingPathfinding(enemy, ship, 20)
-    }
-    
-}
-
-//  game.on_update_interval(2000, spawn_enemies)
-spawn_enemies()
-function enemy_behaviour(enemy: any) {
-    let angle_in_rads = spriteutils.angleFrom(enemy, ship)
-    let angle_in_degrees = spriteutils.radiansToDegrees(angle_in_rads)
-    if (spriteutils.distanceBetween(enemy, ship) > 80) {
-        transformSprites.rotateSprite(enemy, angle_in_degrees + 90)
-    } else {
-        tilesAdvanced.stopFollowingPath(enemy)
-        if (transformSprites.getRotation(enemy) - angle_in_degrees > 1) {
-            transformSprites.changeRotation(enemy, -0.5)
-        }
-        
-        if (randint(1, 100) == 1) {
-            fire(enemy, SpriteKind.enemy_projectile)
-        }
-        
-    }
-    
-}
-
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function destroy_enemy(cannon_ball: Sprite, enemy: Sprite) {
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function hit_fort(fort: Sprite, cannon_ball: Sprite) {
+    let bar = statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, fort)
+    bar.value -= 10
     cannon_ball.destroy()
-    enemy.destroy()
+    if (bar.value < 1) {
+        spawn_treasure(fort.tilemapLocation())
+        fort.destroy()
+    }
+    
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.enemy_projectile, function hit() {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.enemy_projectile, function player_hit(player: Sprite, cannon_ball: Sprite) {
     info.changeLifeBy(-1)
+    cannon_ball.destroy()
     pause(1000)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Enemy, function fix_double_spawn(enemy: Sprite, other_enemy: Sprite) {
+    sprites.allOfKind(SpriteKind.Enemy).pop().destroy()
 })
 function turn_ship() {
     
@@ -123,7 +126,6 @@ game.onUpdate(function tick() {
     turn_ship()
     move()
     for (let enemy of sprites.allOfKind(SpriteKind.Enemy)) {
-        enemy_behaviour(enemy)
+        enemy_fire(enemy)
     }
-    info.changeScoreBy(1)
 })
